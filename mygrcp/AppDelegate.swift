7e5eef2e752ec71e18f7957343c7ac08f658d80c
fugashy <fugashy@icloud.com>
2019-08-12 13:36:48 +0900

    Initial Commit

---
+++ mygrcp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mygrcp
+//
+//  Created by fukukazu kawata on 2019/08/11.
+//  Copyright © 2019 fugashy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mygrcp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mygrcp
+//
+//  Created by fukukazu kawata on 2019/08/11.
+//  Copyright © 2019 fugashy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
